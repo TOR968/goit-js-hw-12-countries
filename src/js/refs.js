711db5846ed78642f06b69d0628fbc1513f794fe
Maxim Turbal <tor968@yahoo.com>
2020-11-01 22:43:35 +0200

    Create refs.js

---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+export const refs = {
+    countriesContainer: document.querySelector('.js-countries__wrapper'),
+    countriesList: document.querySelector('.js-countries__list'),
+    searchInput: document.querySelector('.js-input'),
+    flagOverlay: document.querySelector('.js-flag__overlay'),
+};
