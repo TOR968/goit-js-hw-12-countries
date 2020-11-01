ab58b9cadaf8aedbb2aa49fa137965f36af85858
Maxim Turbal <tor968@yahoo.com>
2020-11-01 22:43:25 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,28 +1,32 @@
-import countriesCardTpl from './countries.hbs';
-import API from './fetchCountries';
-import getRefs from './get-refs';
+import './styles.css';
+import fetchCountries from './js/fetchCountries.js';
+import renderCountriesMarkup from './js/renderCountriesMarkup.js';
+import { refs } from './js/refs';
+import error from './js/pnotify.js'
 
-const refs = getRefs();
+const debounce = require('lodash.debounce');
 
-refs.searchForm.addEventListener('submit', onSearch);
+refs.searchInput.addEventListener('input', debounce(searchInputHandler, 500));
 
-function onSearch(e) {
-  e.preventDefault();
 
-  const form = e.currentTarget;
-  const searchQuery = form.elements.query.value;
 
-  API.fetchCountries(searchQuery)
-    .then(renderCountriesCard)
-    .catch(onFetchError)
-    .finally(() => form.reset());
-}
+function searchInputHandler(e) {
+    let inputValue = e.target.value;
 
-function renderCountriesCard(country) {
-  const markup = countriesCardTpl(country);
-  refs.cardContainer.innerHTML = markup;
-}
+    refs.countriesContainer.innerHTML = '';
+    refs.countriesList.innerHTML = '';
 
-function onFetchError() {
-  alert('try another country');
+    if (inputValue.length > 1) {
+        fetchCountries(inputValue)
+            .then(data => {
+                if (data.length > 0 && data.length <= 10) {
+                    renderCountriesMarkup(data);
+                } else if (data.length > 10) {
+                    error('Too many matches found. Please enter a more specific query!');
+                } else {
+                    error('Sorry. No found country!');
+                }
+            })
+            .catch(error => (console.log(error)));
+    }
 }
